12c0cf278879d35227cbe3efa1af09cafa8e26e7
Golov_Alex <ynd3238@gmail.com>
2025-12-01 23:02:17 +0300

    Добавьте файлы проекта.

---
+++ Golov Lab3/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using Golov_Lab3.UsersСlasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Golov_Lab3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxEmail.Text = "task_code_development@list.ru";
+            textBoxName.Text = "Антон Игоревич";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            // Проверка TextBox на наличие значений
+            if (String.IsNullOrWhiteSpace(textBoxEmail.Text) ||
+                String.IsNullOrWhiteSpace(textBoxName.Text) ||
+                String.IsNullOrWhiteSpace(textBoxSubject.Text) ||
+                String.IsNullOrWhiteSpace(textBoxBody.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            //Ввод данных с формы в объекты ранее созданных классов
+            string smtp = "smtp.mail.ru";
+            //Необходимо ввести свой mail.ru адрес!!! И мвоё ФИО
+            StringPair fromInfo = new StringPair("alexanderg.2@bk.ru", "Голов Александр Юрьевич");
+            //Необходимо ввести свой пароль который вывел mail.ru !!!
+            string password = "MzPsavulsS2d7GKHZ5As";
+
+            StringPair toInfo = new StringPair(textBoxEmail.Text, textBoxName.Text);
+            string subject = textBoxSubject.Text;
+            string body = $"{DateTime.Now}\n" +
+            $"{Dns.GetHostName()}\n" +
+            $"{Dns.GetHostEntry(Dns.GetHostName()).AddressList.First()}\n" +
+            $"{textBoxBody.Text}";
+
+            InfoEmailSending info =
+                new InfoEmailSending(smtp, fromInfo, password, toInfo, subject, body);
+
+            // Отправка данных в виде электронного письма
+            SendingEmail sendingEmail = new SendingEmail(info);
+            sendingEmail.Send();
+
+            // Уведомление для пользователя и очистка всех TextBox
+            MessageBox.Show("Письмо отправлено!");
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+        }
+    }
+}
---
+++ Golov Lab3/UsersСlasses/InfoEmailSending.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golov_Lab3.UsersСlasses
+{
+    public class InfoEmailSending
+    {
+            public InfoEmailSending(string smtpClientAddress,
+            StringPair emailAddressFrom,
+            string emailPassword,
+            StringPair emailAddressTo,
+            string subject,
+            string body)
+            {
+            SmtpClientAddress = String.IsNullOrWhiteSpace(smtpClientAddress) ?
+                throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+                smtpClientAddress;
+            EmailAddressFrom = emailAddressFrom ?? throw new ArgumentNullException(nameof(emailAddressFrom));
+            EmailPassword = String.IsNullOrWhiteSpace(emailPassword) ?
+                throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+                emailPassword;
+            EmailAddressTo = emailAddressTo ?? throw new ArgumentNullException(nameof(emailAddressTo));
+            Subject = subject ?? throw new ArgumentNullException(nameof(Subject));
+            Body = body ?? throw new ArgumentNullException(nameof(Body));
+            }
+
+        public string SmtpClientAddress { get; set; }
+        public StringPair EmailAddressFrom { get; set; }
+        public string EmailPassword { get; set; }
+        public StringPair EmailAddressTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class StringPair
+        {
+            public StringPair(string emailAdress, string name)
+            {
+                EmailAdress = String.IsNullOrWhiteSpace(emailAdress) ?
+                    throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+                    emailAdress;
+                Name = String.IsNullOrWhiteSpace(name) ?
+                    throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+                    name;
+            }
+
+            public string EmailAdress { get; set; }
+            public string Name { get; set; }
+    }
+}
